32094bb61ac7331e7bf67e9dab67d54e84dfd481
Alex Groce <agroce@gmail.com>
2016-04-19 14:12:56 -0700

    better comment

--- showgitinclass/dominion.c
+++ showgitinclass/dominion.c
@@ -34,7 +34,7 @@ int* kingdomCards(int k1, int k2, int k3, int k4, int k5, int k6, int k7,
   return k;
 }
 
-/* The following function is pretty important. */
+/* The following function is pretty INSANELY important. */
 
 int initializeGame(int numPlayers, int kingdomCards[10], int randomSeed,
 		   struct gameState *state) {
